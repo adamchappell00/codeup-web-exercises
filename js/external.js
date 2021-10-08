0c73a3ea510374518d0f495514e449381d310f43
adamchappell00 <adam.chappell00@gmail.com>
2021-10-08 11:41:40 -0500

    external js creation

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("Hello from External JavaScript")
+
+alert("This is an alert.");
+
+var confirmMe = confirm('Can you please confirm this confirm command?');
+console.log(confirmMe);
+
+var userPromptResponse = prompt('Please respond to my prompt:');
+console.log('The user entered: ' + userPromptResponse);
