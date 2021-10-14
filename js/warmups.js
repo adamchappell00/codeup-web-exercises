ff1ae88fa8fc5444ad55f2a51e7308ad77311cfe
adamchappell00 <adam.chappell00@gmail.com>
2021-10-14 09:01:56 -0500

    14 October Warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,16 +1,18 @@
 "use strict";
-//Current Exercise Goes HERE
 console.log(Date());
-function gimmeSeven(){
-    return 7;
-}
-console.log(gimmeSeven());
+//Leave Line 1-3!!!
+
+//Current Exercise Goes HERE
+var hadBreakfast = Boolean(Math.round(Math.random()));
+var hadBreakfastMessage = hadBreakfast ? "I had an amazing breakfast today" : " I am hungry :("
+
+console.log(hadBreakfastMessage);
 
 /* Previous Exerises Here
 
 
 
-
+--Thurs October 14--
 
 --Weds October 13--
 console.log(Date());
