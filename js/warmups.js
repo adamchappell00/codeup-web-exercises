f1c8615df07604717829b473d1d148b957a29456
adamchappell00 <adam.chappell00@gmail.com>
2021-10-15 10:26:13 -0500

    15 October Warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,21 +1,60 @@
 "use strict";
 console.log(Date());
-//Leave Line 1-3!!!
 
-//Current Exercise Goes HERE
-var hadBreakfast = Boolean(Math.round(Math.random()));
-var hadBreakfastMessage = hadBreakfast ? "I had an amazing breakfast today" : " I am hungry :("
+//Leave Line 1-3 Above, Put Current Exercise Below!
+function typeCheck(input){
+    if (typeof input === 'string'){
+        return "Haha, it's a string of string!"
+    } else {
+        return typeof input;
+    }
+}
+// Instructors are not kind with our brains on the following 4 tests
+// console.log(typeCheck(‘’));
+// console.log(typeCheck(isNaN()));
+// console.log(typeCheck(!false * 3));
+// console.log(typeCheck(4 + ‘4’));
+var test1 = '';
+var test2 = isNaN();
+var test3 = !false * 3;
+var test4 = 4 + '4';
 
-console.log(hadBreakfastMessage);
+console.log(typeCheck(test1));
+console.log(typeCheck(test2));
+console.log(typeCheck(test3));
+console.log(typeCheck(test4));
 
 /* Previous Exerises Here
+--Fri October 15--
+unction typeCheck(input){
+    if (typeof input === 'string'){
+        return "Haha, it's a string of string!"
+    } else {
+        return typeof input;
+    }
+}
+// Instructors are not kind with our brains on the following 4 tests
+// console.log(typeCheck(‘’));
+// console.log(typeCheck(isNaN()));
+// console.log(typeCheck(!false * 3));
+// console.log(typeCheck(4 + ‘4’));
+var test1 = '';
+var test2 = isNaN();
+var test3 = !false * 3;
+var test4 = 4 + '4';
 
-
+console.log(typeCheck(test1));
+console.log(typeCheck(test2));
+console.log(typeCheck(test3));
+console.log(typeCheck(test4));
 
 --Thurs October 14--
+var hadBreakfast = Boolean(Math.round(Math.random()));
+var hadBreakfastMessage = hadBreakfast ? "I had an amazing breakfast today" : " I am hungry :(";
+console.log(hadBreakfast);
+console.log(hadBreakfastMessage);
 
 --Weds October 13--
-console.log(Date());
 function gimmeSeven(){
     return 7;
 }
