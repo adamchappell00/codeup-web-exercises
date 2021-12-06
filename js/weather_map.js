ceaefba5392744887cff04fd6c194d3c0b56a18f
adamchappell00 <adam.chappell00@gmail.com>
2021-12-06 10:33:01 -0600

    basic 5-Day weather data added to HTML, needs styling and additional info

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,5 +8,14 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
     appid: OPEN_WEATHER_KEY
 }).done(function(data){
     console.log(data);
+    var fiveDayHtml= '';
+    for(var i=0 ; i < 5; i++){
+        fiveDayHtml += '<div class="day"><div class="date">' + new Date(data.daily[i].dt * 1000)+ '</div>' +
+            '<div class="weather-desc">' + data.daily[i].weather[0].main + '<div class="weather-subtype-desc">' +
+            data.daily[i].weather[0].description + '</div></div>' +
+            '<div class="temp">High: '+ data.daily[i].temp.max +'<br>Low: '+ data.daily[i].temp.min +'</div>'
+        fiveDayHtml += '</div>'
+    }
+    $('#five-day').html(fiveDayHtml);
     console.log(new Date(data.daily[0].dt * 1000));
 });
