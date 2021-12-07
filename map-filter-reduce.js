b96113402c6bae0a23fa71a7b9d86037963e6b2d
adamchappell00 <adam.chappell00@gmail.com>
2021-12-07 14:53:47 -0600

    starting es6 map-filter-reduce exercise

---
+++ map-filter-reduce.js
@@ -0,0 +1,47 @@
+const users2 = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const versatile = users2.filter(user => user.languages.length >= 3)
+let versatileNames = [];
+
+//DESTRUCTURE ME
+for(let user of versatile){
+    versatileNames.push(user.name);
+}
+let mapFilterReduce = `Filter by 3 Languages: ${versatile.name}`;
+$('#map-filter-reduce').html(mapFilterReduce);
