415bad4316ae1d837cee2cf4cb8b22e4197a5e57
adamchappell00 <adam.chappell00@gmail.com>
2021-12-09 14:10:42 -0600

    last github push complete

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,16 @@
 
+// Exstablish Username: Allow for later feature of updating or entering in a user
 let userName = 'adamchappell00';
-const myLastPush = (user)=> fetch(`https://api.github.com/users/${user}/events`,
+
+// Establish the function with fetch/key
+const userLastPush = (user) => fetch(`https://api.github.com/users/${user}/events`,
     {headers: {'Authorization': 'GITHUB_KEY'}})
-    .then(resp => resp.json());
-//    .then(resp => {resp.filter(event => event.type === 'push')});
-console.log(myLastPush(userName));
- //   .then()
+    // Take the result into an object, find all of the Push Events
+    .then(resp => resp.json())
+    .then(events => events.filter((event) => event.type === 'PushEvent'))
+    // Log the data+time of the most recent push (index of 0)
+    .then(pushes => console.log(pushes[0].created_at));
+//call the function
+userLastPush(userName);
+
+
